cba2d166d54ddcb858ed696ee3f956b462af802b
femunoz <felipe.munoz.dev@gmail.com>
2023-12-18 14:50:15 -0300

    ejemplo mejorado clase por zoom

--- templates.py
+++ templates.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def bienvenida():
+    return "<html><body><h2>Bienvenid@!</h2></body><html>"
+
+@app.route('/hello/')
+def hello2():
+    return render_template('plantilla2.html', name="NN")
 @app.route('/hello/<name>')
-def hello(name=None):
+def hello(name="Juanito"):
     return render_template('plantilla2.html', name=name)
